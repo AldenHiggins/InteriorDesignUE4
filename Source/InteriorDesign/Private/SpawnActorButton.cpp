89d1924d3168d70ccd874e20a6356f80b1fc2751
Alden Higgins <johnaldenhiggins@gmail.com>
2016-04-14 17:13:41 -0700

    Small cleanup of spawn actor button

--- Source/InteriorDesign/Private/SpawnActorButton.cpp
+++ Source/InteriorDesign/Private/SpawnActorButton.cpp
@@ -3,27 +3,23 @@
 #include "InteriorDesign.h"
 #include "SpawnActorButton.h"
 
-
 // Sets default values
 ASpawnActorButton::ASpawnActorButton()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
 // Called when the game starts or when spawned
 void ASpawnActorButton::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 // Called every frame
 void ASpawnActorButton::Tick( float DeltaTime )
 {
 	Super::Tick( DeltaTime );
-
 }
 
 void ASpawnActorButton::hoverAction()
@@ -39,7 +35,4 @@ void ASpawnActorButton::releaseAction()
 void ASpawnActorButton::pressAction()
 {
 	UE_LOG(LogTemp, Warning, TEXT("Looks like you pressed a button you cool dude ;)"));
-	//UE_LOG(LogTemp, Warning, TEXT("Release action"));
 }
-
-
